0ebf7942b1da70e3341dfc82851a3acacff5c7ad
fazlaejderhasiolanvarmi <burakceltik@hotmail.com>
2022-07-16 14:43:56 +0300

    Completed RandomWord.java according to the assignment

--- src/RandomWord.java
+++ src/RandomWord.java
@@ -1,6 +1,20 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
     // Week 1 - Course Introduction
     public static void main(String[] args){
+        String champion = args[0];
 
+        for(int i = 1; i < args.length; i++){
+            String challenger = args[i];
+
+            if(StdRandom.bernoulli(1.0 / args.length)){
+                champion = challenger;
+            }
+        }
+
+        System.out.println(champion);
     }
 }
