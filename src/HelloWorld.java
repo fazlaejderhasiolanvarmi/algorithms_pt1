942e687b5cbc4c7ba6bcd2e3ac141cc4665abaf7
fazlaejderhasiolanvarmi <burakceltik@hotmail.com>
2022-07-16 13:48:08 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    // Week 1 - Course Introduction
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ src/RandomWord.java
@@ -0,0 +1,6 @@
+public class RandomWord {
+    // Week 1 - Course Introduction
+    public static void main(String[] args){
+
+    }
+}
